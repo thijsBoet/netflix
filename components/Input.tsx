98c3b7c880a483856a339b066ce63dce82310cf6
Matthijs Boet <m.boet@chello.nl>
2023-07-10 19:20:05 +0200

    build login sign up UI on auth route

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Input = () => {
+interface InputProps {
+  id: string;
+  onChange: any;
+  value: string;
+  label: string;
+  type?: string;
+}
+
+const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
   return (
-		<div className='relative'>
-			<input
-				className='
+    <div className="relative">
+      <input
+        onChange={onChange}
+        value={value}
+        type={type}
+        id={id}
+        className="
         block
         rounded-md
         px-6
@@ -19,16 +31,30 @@ const Input = () => {
         focus:ring-0
         peer
         invalid:border-b-1
-        '
-				placeholder=' '
-			/>
-			<label
-				htmlFor='email'
-				className='absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0] left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-3'>
-				Email
-			</label>
-		</div>
-  );
+        "
+        placeholder=" " 
+      />
+      <label 
+        htmlFor={id} 
+        className="
+        absolute 
+        text-md
+      text-zinc-400
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-10 
+        origin-[0] 
+        left-6
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      ">{label}</label>
+    </div>
+  )
 }
 
-export default Input
+export default Input;
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,24 +1,86 @@
-import Input from "@/components/Input";
-import Image from "next/image";
+import Input from '@/components/Input';
+import Image from 'next/image';
+import { useCallback, useState } from 'react';
 
 const Auth = () => {
-  return (
-    <div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover bg-[url('/images/hero.jpg')]">
-      <div className="bg-black w-full h-full lg:bg-opacity-50">
-        <div className="px-12 py-5">
-          <Image src="/images/logo.png" alt="Logo" className="h-12" width={178} height={48}/>
-        </div>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 rounded-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-            <div className="flex flex-col gap-4">
-              <Input />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+	const [email, setEmail] = useState('');
+	const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const [variant, setVariant] = useState('login')
 
-export default Auth
+  const toggleVariant = useCallback(() => { 
+    setVariant((currentVariant) => currentVariant === 'login' ? 'signup' : 'login')
+  }, []);
+  
+
+	return (
+		<div className="relative h-full w-full bg-no-repeat bg-center bg-fixed bg-cover bg-[url('/images/hero.jpg')]">
+			<div className='bg-black w-full h-full lg:bg-opacity-50'>
+				<div className='px-12 py-5'>
+					<Image
+						src='/images/logo.png'
+						alt='Logo'
+						className='h-12'
+						width={178}
+						height={48}
+					/>
+				</div>
+				<div className='flex justify-center'>
+					<div className='bg-black bg-opacity-70 p-16 self-center mt-2 lg:w-2/5 rounded-md w-full'>
+						<h2 className='text-white text-4xl mb-8 font-semibold'>
+							{variant === 'login' ? 'Sign In' : 'Register'}
+						</h2>
+						<div className='flex flex-col gap-4'>
+							{variant === 'signup' && (
+								<Input
+									label='Username'
+									onChange={(e: any) =>
+										setName(e.target.value)
+									}
+									id='username'
+									type='text'
+									value={name}
+								/>
+							)}
+
+							<Input
+								label='Email'
+								onChange={(e: any) => setEmail(e.target.value)}
+								id='email'
+								type='email'
+								value={email}
+							/>
+							<Input
+								label='Password'
+								onChange={(e: any) =>
+									setPassword(e.target.value)
+								}
+								id='password'
+								type='password'
+								value={password}
+							/>
+						</div>
+						<button className=' bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition'>
+							{variant === 'login' ? 'Sign In' : 'Sign Up'}
+						</button>
+						<p className='text-neutral-500 mt-12'>
+							{variant === 'login'
+								? 'First time using Netflix?'
+								: 'Already have an account?'}
+							<span
+								className='text-white ml-1.5 hover:underline cursor-pointer'
+								onClick={toggleVariant}>
+								{variant === 'login'
+									? 'Create an account?'
+									: 'Login'}
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Auth;
